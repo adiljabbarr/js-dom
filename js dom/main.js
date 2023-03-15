a45e9c7c41e40d2c16d09011cec89ec7e8f00c48
adiljabbarr <126657224+adiljabbarr@users.noreply.github.com>
2023-03-15 11:59:18 +0400

    js dom tapsiriq
    tam alindira bilmedim;(

---
+++ js dom/main.js
@@ -0,0 +1,6 @@
+let div = document.getElementById('div1');
+const p = document.getElementById('p1');
+        console.log(p);
+        div.id = 'content';
+        div.innerHTML = '<h1>A Bootstrap 5 Starter Template<h1>';
+        document.body.appendChild(div);
